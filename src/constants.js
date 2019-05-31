88dbca306c17aaee29b6018b64c3da953e95ca71
Kenny Huynh <kennyhuynh@TSGAdmins-MBP.T-mobile.com>
2019-05-31 10:41:47 -0700

    changed resume url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const GITHUB_URL = 'https://github.com/kennyhuynh125';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/kennyhuynh125/';
-export const RESUME_URL = 'https://drive.google.com/file/d/1K0ERxW_iU0gjtk9D-j4OZzQJ9zwV7VXO/view?usp=sharing';
+export const RESUME_URL = 'https://drive.google.com/file/d/1LEXa0Y1lyBOkUIk0rAGrYzywIEPHQBUj/view?usp=sharing';
